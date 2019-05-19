7cf7de62188b2b0b4202ccec8c231bfb497b330e
Adrian Lindberg <adrlin@student.chalmers.se>
2019-05-19 21:36:23 +0200

    Updated snackbar

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <v-app>
-    <v-content>
+    <v-content style="padding: 15px;">
       <v-container fluid fill-height>
         <router-view/>
+        <v-layout
+          column
+          wrap
+          justify-end
+          align-center
+          style="position: fixed; bottom: 30px; padding: 0px; margin-left: -15px; margin-right: 15px;"
+        ></v-layout>
       </v-container>
     </v-content>
   </v-app>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -10,6 +10,12 @@
         >
         <v-btn style="font-family: Open Sans; width: 100%;" round large to="/teamsettings">Spela</v-btn>
       </div>
+      <div id="snackbar">
+        A new version is available.
+        <strong>
+          <a id="reload">REFRESH</a>
+        </strong>
+      </div>
     </v-layout>
   </v-container>
 </template>
@@ -21,6 +27,51 @@ h1 {
   font-family: "Lilita One";
   font-size: 4.5em;
   text-align: center;
+}
+#snackbar {
+  visibility: show;
+  width: 90%;
+  max-width: 400px;
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  font-family: Open Sans;
+  border-radius: 15px;
+  padding: 16px;
+  position: fixed;
+  bottom: 30px;
+  margin: 0;
+  border: 0.5px solid #444;
+}
+#snackbar a {
+  color: #70b1f1;
+  font-family: Open Sans;
+  margin-left: 10px;
+}
+#snackbar.show {
+  visibility: visible;
+  -webkit-animation: fadein 0.5s;
+  animation: fadein 0.5s;
+}
+@-webkit-keyframes fadein {
+  from {
+    bottom: 0;
+    opacity: 0;
+  }
+  to {
+    bottom: 30px;
+    opacity: 1;
+  }
+}
+@keyframes fadein {
+  from {
+    bottom: 0;
+    opacity: 0;
+  }
+  to {
+    bottom: 30px;
+    opacity: 1;
+  }
 }
 </style>
 
